696ef7125b771701e372890fe31e402664297498
Zijun <s410126080@gmail.com>
2023-03-29 07:33:43 +0800

    增加、修改、刪除 brand 資料

--- jdbc-demo/src/com/Kent/example/BrandTest.java
+++ jdbc-demo/src/com/Kent/example/BrandTest.java
@@ -75,4 +75,153 @@ public class BrandTest {
         pstmt.close();
         conn.close();
     }
+
+    /**
+     * 插入
+     * 1. SQL: insert into tb_brand(brand_name, company_name, ordered, description, status) values (?, ?, ?, ?, ?);
+     * 2. 參數: 除了 id 之外的所有參數訊息
+     * 3. 結果: boolean
+     * @throws Exception
+     */
+    @Test
+    public void testAdd() throws Exception {
+        // 模擬接收前端提交的參數
+        String brandName = "香飄飄";
+        String companyName = "香飄飄";
+        int ordered = 1;
+        String description = "繞地球一圈";
+        int status = 1;
+
+        // 1. 取得 Connection
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("./src/druid.properties"));
+
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+        Connection conn = dataSource.getConnection();
+
+        // 2. 定義 SQL
+        String sql = "insert into tb_brand(brand_name, company_name, ordered, description, status) values (?, ?, ?, ?, ?);";
+
+        // 3. 獲取 pstmt 物件
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        // 4. 設置參數
+        pstmt.setString(1, brandName);
+        pstmt.setString(2, companyName);
+        pstmt.setInt(3, ordered);
+        pstmt.setString(4, description);
+        pstmt.setInt(5, status);
+
+        // 5. 執行 SQL
+        int count = pstmt.executeUpdate(); // 影響的行數
+
+        // 6. 處理結果
+        System.out.println(count > 0);
+
+        // 7. 釋放資源
+        pstmt.close();
+        conn.close();
+    }
+
+    /**
+     * 修改
+     * 1. SQL:
+     update tb_brand
+        set brand_name = ?,
+        company_name = ?,
+        ordered = ?,
+        description = ?,
+        status = ?
+     where id = ?
+     * 2. 參數: 需要所有的參數
+     * 3. 結果: boolean
+     * @throws Exception
+     */
+    @Test
+    public void testUpdate() throws Exception {
+        // 模擬接收前端提交的參數
+        String brandName = "香飄飄";
+        String companyName = "香飄飄";
+        int ordered = 1000;
+        String description = "繞地球三圈";
+        int status = 1;
+        int id = 4;
+
+        // 1. 取得 Connection
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("./src/druid.properties"));
+
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+        Connection conn = dataSource.getConnection();
+
+        // 2. 定義 SQL
+        String sql = "update tb_brand\n" +
+                "        set brand_name = ?,\n" +
+                "        company_name = ?,\n" +
+                "        ordered = ?,\n" +
+                "        description = ?,\n" +
+                "        status = ?\n" +
+                "     where id = ?";
+
+        // 3. 獲取 pstmt 物件
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        // 4. 設置參數
+        pstmt.setString(1, brandName);
+        pstmt.setString(2, companyName);
+        pstmt.setInt(3, ordered);
+        pstmt.setString(4, description);
+        pstmt.setInt(5, status);
+        pstmt.setInt(6,id);
+
+        // 5. 執行 SQL
+        int count = pstmt.executeUpdate(); // 影響的行數
+
+        // 6. 處理結果
+        System.out.println(count > 0);
+
+        // 7. 釋放資源
+        pstmt.close();
+        conn.close();
+    }
+
+    /**
+     * 刪除
+     * 1. SQL:
+     delete from tb_brand where id = ?
+     * 2. 參數: 只需要 id
+     * 3. 結果: boolean
+     * @throws Exception
+     */
+    @Test
+    public void testDeleteById() throws Exception {
+        // 模擬接收前端提交的參數
+        int id = 6;
+
+        // 1. 取得 Connection
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("./src/druid.properties"));
+
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+        Connection conn = dataSource.getConnection();
+
+        // 2. 定義 SQL
+        String sql = "delete from tb_brand where id = ?";
+
+        // 3. 獲取 pstmt 物件
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        // 4. 設置參數
+        pstmt.setInt(1,id);
+
+        // 5. 執行 SQL
+        int count = pstmt.executeUpdate(); // 影響的行數
+
+        // 6. 處理結果
+        System.out.println(count > 0);
+
+        // 7. 釋放資源
+        pstmt.close();
+        conn.close();
+    }
 }
